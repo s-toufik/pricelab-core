24eec458ba24937f4df26cdb319aa3f74bc24898
Toufik Saouchi <toufik.saouchi@gmail.com>
2026-03-29 15:01:00 +0200

    validate timestamp

--- src/domain/model/base_market_data.py
+++ src/domain/model/base_market_data.py
@@ -1,3 +1,4 @@
+import datetime
 from dataclasses import dataclass
 
 @dataclass(frozen=True, slots=True)
@@ -5,3 +6,14 @@ class BaseMarketData:
     source: str
     symbol: str
     timestamp: str
+
+    def __post_init__(self):
+        self._validate_timestamp()
+
+    def _validate_timestamp(self):
+        if not isinstance(self.timestamp, str):
+            raise TypeError("Timestamp must be a string")
+        try:
+            datetime.datetime.fromisoformat(self.timestamp)
+        except:
+            raise ValueError
