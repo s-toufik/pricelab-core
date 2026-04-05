e8ce698310aacdccd81b3753eeac7f97e1c990e2
Toufik Saouchi <toufik.saouchi@gmail.com>
2026-04-05 13:34:16 +0200

    correct pricelab package namespace in src

---
+++ src/pricelab_core/adapter/outbound/serializer/dictionary_serializer.py
@@ -0,0 +1,17 @@
+from dataclasses import asdict
+from typing import TypeVar, Type
+
+from src.pricelab_core.application.port.outbound.serializer.serializer import Serializer
+
+T = TypeVar("T")
+
+
+class DictionarySerializer(Serializer):
+
+    @staticmethod
+    def serialize(inputs: T) -> dict:
+        return asdict(inputs)
+
+    @staticmethod
+    def deserialize(inputs: dict, cls: Type[T]) -> T:
+        return cls(**inputs)
